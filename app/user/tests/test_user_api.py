2393f871e414906aa9c3b9a1ac96133cf7b0cfd4
Aamod Pant Balekundri <aamod@1x-nat-vl932-172-30-198-102.wireless.umass.edu>
2021-07-15 17:04:33 -0400

    Add user management tests and endpoints

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,4 +1,3 @@
-# from os import stat
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -9,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -100,3 +100,51 @@ class PublicUserAPITest(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserAPITest(TestCase):
+    """Test user API (authenticated)"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@gmail.com',
+            password='password',
+            name='test'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_sucess(self):
+        """Test retrieving profile for logged in used"""
+
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on the me URL"""
+
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated users"""
+
+        info = {
+            'name': 'New Name',
+            'password': 'newpass'
+        }
+        res = self.client.patch(ME_URL, info)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, info['name'])
+        self.assertTrue(self.user.check_password(info['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
